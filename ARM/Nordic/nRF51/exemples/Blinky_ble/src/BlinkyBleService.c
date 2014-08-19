a6ff0e4e6b74b276cc641f8697d7e99e240b5cec
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2014-08-19 17:34:34 -0400

    Blinky over BLE exemple

---
+++ ARM/Nordic/nRF51/exemples/Blinky_ble/src/BlinkyBleService.c
@@ -0,0 +1,217 @@
+/*--------------------------------------------------------------------------
+File   : BlinkyBleService.c
+
+Author : Hoang Nguyen Hoan          Aug. 18, 2014
+
+Desc   : Blinky over custom BLE exemple.
+		 Custom BLE service implementation
+
+Copyright (c) 2014, I-SYST inc., all rights reserved
+
+Permission to use, copy, modify, and distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies, and none of the
+names : I-SYST or its contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+For info or contributing contact : hnhoan at i-syst dot com
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----------------------------------------------------------------------------
+Modified by          Date              Description
+
+----------------------------------------------------------------------------*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "istddef.h"
+#include "nordic_common.h"
+#include "nrf.h"
+#include "nrf_assert.h"
+#include "nrf51_bitfields.h"
+#include "ble.h"
+#include "ble_hci.h"
+#include "ble_srv_common.h"
+#include "ble_advdata.h"
+#include "ble_hids.h"
+#include "ble_bas.h"
+#include "ble_dis.h"
+#include "ble_conn_params.h"
+#include "device_manager.h"
+#include "ble_error_log.h"
+#include "ble_debug_assert_handler.h"
+#include "app_error.h"
+#include "app_timer.h"
+#include "softdevice_handler.h"
+#include "BlinkyBleService.h"
+
+
+/**@brief Connect event handler.
+ *
+ * @param[in]   p_blueios    EkoCC Service structure.
+ * @param[in]   p_ble_evt   Event received from the BLE stack.
+ */
+static void on_connect(ble_blinkys_t * p_blinkys, ble_evt_t * p_ble_evt)
+{
+	p_blinkys->conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+}
+
+
+static void on_disconnect(ble_blinkys_t * p_blinkys, ble_evt_t * p_ble_evt)
+{
+    UNUSED_PARAMETER(p_ble_evt);
+    p_blinkys->conn_handle = BLE_CONN_HANDLE_INVALID;
+}
+
+
+void ble_blinkys_on_ble_evt(ble_blinkys_t * p_blinkys, ble_evt_t * p_ble_evt)
+{
+    switch (p_ble_evt->header.evt_id)
+    {
+        case BLE_GAP_EVT_CONNECTED:
+            on_connect(p_blinkys, p_ble_evt);
+            break;
+
+        case BLE_GAP_EVT_DISCONNECTED:
+            on_disconnect(p_blinkys, p_ble_evt);
+            break;
+
+        case BLE_GATTS_EVT_WRITE:
+        	{
+        		ble_gatts_evt_write_t * p_evt_write = &p_ble_evt->evt.gatts_evt.params.write;
+
+        		if ((p_evt_write->handle == p_blinkys->blinky_char_handles.value_handle) &&
+        	       (p_evt_write->len > 0) &&
+        	       (p_blinkys->write_handler != NULL))
+        		{
+        			p_blinkys->write_handler(p_blinkys, p_evt_write->data, p_evt_write->len);
+        		}
+        	}
+            break;
+
+        default:
+            break;
+    }
+}
+
+static uint32_t blinky_char_add(ble_blinkys_t * p_blinkys, const ble_blinkys_init_t * p_blinkys_init)
+{
+    ble_gatts_char_md_t char_md;
+    ble_gatts_attr_md_t cccd_md;
+    ble_gatts_attr_t    attr_char_value;
+    ble_uuid_t          ble_uuid;
+    ble_gatts_attr_md_t attr_md;
+
+
+    memset(&cccd_md, 0, sizeof(cccd_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&cccd_md.write_perm);
+
+    cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+
+    memset(&char_md, 0, sizeof(char_md));
+
+    char_md.char_props.read   = 1;
+    char_md.char_props.write  = 0;
+    char_md.char_props.notify = 1;
+    char_md.char_props.write_wo_resp = 1;
+    char_md.p_char_user_desc  = NULL;
+    char_md.p_char_pf         = NULL;
+    char_md.p_user_desc_md    = NULL;
+    char_md.p_cccd_md         = &cccd_md;
+    char_md.p_sccd_md         = NULL;
+
+    ble_uuid.type = p_blinkys->uuid_type;
+    ble_uuid.uuid = BLINKY_UUID_CHAR;
+
+    memset(&attr_md, 0, sizeof(attr_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+
+    attr_md.vloc       = BLE_GATTS_VLOC_STACK;
+    attr_md.rd_auth    = 0;
+    attr_md.wr_auth    = 0;
+    attr_md.vlen       = 0;
+
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+
+    attr_char_value.p_uuid       = &ble_uuid;
+    attr_char_value.p_attr_md    = &attr_md;
+    attr_char_value.init_len     = 0;
+    attr_char_value.init_offs    = 0;
+    attr_char_value.max_len      = 20;
+    attr_char_value.p_value      = NULL;
+
+    return sd_ble_gatts_characteristic_add(p_blinkys->service_handle, &char_md,
+                                           &attr_char_value,
+                                           &p_blinkys->blinky_char_handles);
+}
+
+uint32_t ble_blinkys_init(ble_blinkys_t * p_blinkys, const ble_blinkys_init_t * p_blinkys_init)
+{
+    uint32_t   err_code;
+    ble_uuid_t ble_uuid;
+
+    // Initialize service structure
+    p_blinkys->conn_handle		= BLE_CONN_HANDLE_INVALID;
+    p_blinkys->write_handler 	= p_blinkys_init->write_handler;
+
+    // Add base UUID to softdevice's internal list.
+    ble_uuid128_t base_uuid = {BLINKY_UUID_BASE};
+    err_code = sd_ble_uuid_vs_add(&base_uuid, &p_blinkys->uuid_type);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+
+    ble_uuid.type = p_blinkys->uuid_type;
+    ble_uuid.uuid = BLINKY_UUID_SERVICE;
+
+    err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY, &ble_uuid, &p_blinkys->service_handle);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+
+    err_code = blinky_char_add(p_blinkys, p_blinkys_init);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+
+    return NRF_SUCCESS;
+}
+
+uint32_t ble_blinkys_on_data_change(ble_blinkys_t * p_blinkys)
+{
+    ble_gatts_hvx_params_t params;
+    uint8_t data;
+    uint16_t len = sizeof(data);
+
+    memset(&params, 0, sizeof(params));
+    params.type = BLE_GATT_HVX_NOTIFICATION;
+    params.handle = p_blinkys->blinky_char_handles.value_handle;
+    params.p_data = &data;
+    params.p_len = &len;
+
+    return sd_ble_gatts_hvx(p_blinkys->conn_handle, &params);
+}
+
+
+
+
+
---
+++ ARM/Nordic/nRF51/exemples/Blinky_ble/src/BlinkyBleService.h
@@ -0,0 +1,90 @@
+/*--------------------------------------------------------------------------
+File   : BlinkyBleService.h
+
+Author : Hoang Nguyen Hoan          Aug. 18, 2014
+
+Desc   : Blinky over custom BLE exemple.
+		 Custom BLE service implementation
+
+Copyright (c) 2014, I-SYST inc., all rights reserved
+
+Permission to use, copy, modify, and distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies, and none of the
+names : I-SYST or its contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+For info or contributing contact : hnhoan at i-syst dot com
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----------------------------------------------------------------------------
+Modified by          Date              Description
+
+----------------------------------------------------------------------------*/
+
+#ifndef __BLINKYBLESERVICE_H__
+#define __BLINKYBLESERVICE_H__
+
+#include <stdint.h>
+
+// UUID : 00000000-59a4-4d2b-8479-8bbdbcf77fcd
+#define BLINKY_UUID_BASE {0xcd, 0x7f, 0xf7, 0xbc, 0xbd, 0x8b, 0x79, 0x84, 0x2b, 0x4d, 0xa4, 0x59, 0x0, 0x0, 0x0, 0x0}
+#define BLINKY_UUID_SERVICE			1
+#define BLINKY_UUID_CHAR			2
+
+
+#define BLINKY_CMD_OFF			0
+#define BLINKY_CMD_ON			1
+#define BLINKY_CMD_CFGOUTPUT	2
+#define BLINKY_CMD_CFGINPUT 	3
+
+// Data info
+typedef struct blinky_cmd {
+	uint8_t		iopin;		// I/O pin # to control
+	uint8_t		cmd;		// Command
+} BLINKY_CMD;
+
+typedef struct ble_blinkys_s ble_blinkys_t;
+
+typedef void (*ble_blinkys_write_handler_t) (ble_blinkys_t * p_blinkys, uint8_t *data, int len);
+
+struct ble_blinkys_s {
+    uint16_t                    service_handle;
+    ble_gatts_char_handles_t    blinky_char_handles;
+    uint8_t                     uuid_type;
+    uint8_t                     current_led_state;
+    uint16_t                    conn_handle;
+    bool                        is_notifying;
+    ble_blinkys_write_handler_t	write_handler;
+};
+
+typedef struct
+{
+    ble_blinkys_write_handler_t	write_handler;
+} ble_blinkys_init_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint32_t ble_blinkys_init(ble_blinkys_t * p_blinkys, const ble_blinkys_init_t * p_blinkys_init);
+void ble_blinkys_on_ble_evt(ble_blinkys_t * p_blinkys, ble_evt_t * p_ble_evt);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __BLINKYBLESERVICE_H__
+
