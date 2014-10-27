c496421e78549521be680796258c49ac696d03fa
Hoan Hoang <hnhoan@i-syst.com>
2014-10-27 08:08:51 -0400

    initial commit

---
+++ src/CppRuntimeOverload.cpp
@@ -0,0 +1,53 @@
+/*--------------------------------------------------------------------------
+File   : CppRuntimeOverload.cpp
+
+Author : Hoang Nguyen Hoan          Oct. 16, 2014
+
+Desc   : C++ runtime overloading to remove the stdc++ library
+		 in order to reduce compiled size to fit in smaller memory
+		 footprint
+
+Copyright (c) 2014, I-SYST inc., all rights reserved
+
+Permission to use, copy, modify, and distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies, and none of the
+names : I-SYST or its contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+For info or contributing contact : hnhoan at i-syst dot com
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----------------------------------------------------------------------------
+Modified by          Date              Description
+
+----------------------------------------------------------------------------*/
+
+#include <stdlib.h>
+
+
+void *operator new(size_t size) throw()
+{
+	return malloc(size);
+}
+
+void operator delete(void *p) throw()
+{
+	free(p);
+}
+
+extern "C" void __cxa_pure_virtual()
+{
+	while(1);	// Freeze on call to an unimplemented pure virtual
+}
