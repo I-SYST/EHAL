255a29791e3a8c6d3bd3a60721b405ddc5b0facb
Hoan Hoang <hnhoan@i-syst.com>
2015-03-22 21:23:18 -0400

    Initial commit : Implementing standard device I/O for stdio.  Allows use of printf, open, etc...

---
+++ include/stddev.h
@@ -0,0 +1,101 @@
+/*--------------------------------------------------------------------------
+File   : stddev.h
+
+Author : Hoang Nguyen Hoan          Mar. 8, 2015
+
+Desc   : Implementation of stdio device remapping
+
+Copyright (c) 2015, I-SYST, all rights reserved
+
+Permission to use, copy, modify, and distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies, and none of the
+names : I-SYST or its contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+For info or contributing contact : hnhoan at i-syst dot com
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----------------------------------------------------------------------------
+Modified by          Date              Description
+
+----------------------------------------------------------------------------*/
+#ifndef __SDTDEV_H__
+#define __STDDEV_H__
+
+#include <stdint.h>
+#include <unistd.h>
+
+#define STDDEV_MAX				6		// Max number of standard device
+#define STDDEV_NAME_MAX			8
+#define STDDEV_FATFS_FILENO		3
+#define STDDEV_USER_FILENO		4		// Start of custom user fileno idx
+
+// open
+typedef int (*STDDEVOPEN)(void *pDevObj, const char *pDevName, int Flags, int Mode);
+// close
+typedef int (*STDDEVCLOSE)(void *pDevObj, int Handle);
+// Read/Write
+typedef int (*STDDEVRW)(void *pDevObj, int Handle, uint8_t *pBuff, size_t Len);
+// seek
+typedef int (*STDDEVSEEK)(void *pDevObj, int Handle, int Offset);
+
+#pragma pack(push, 4)
+
+typedef struct {
+	char 		Name[STDDEV_NAME_MAX];	// Device name
+	void 		*pDevObj;	// Device object
+	STDDEVOPEN	Open;		// Pointer to Open function
+	STDDEVCLOSE Close;		// Pointer to Close function
+	STDDEVRW	Read;		// Pointer to Read function
+	STDDEVRW	Write;		// Pointer to Write function
+	STDDEVSEEK	Seek;		// Pointer to Seek function
+} STDDEV;
+
+#pragma pack(pop)
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Install block device into stdio syscall
+ *
+ * @param	pDev 	: Pointer to standard device descriptor structure
+ * 			MapIp	: Mapping id
+ * 						STDIN_FILENO 	- to replace stdin
+ * 						STDOUT_FILENO 	- to replace stdout
+ * 						STDERR_FILENO	- to replace stderr
+ * 						STDDEV_FATFS_FILENO - for stdio.h filesystem
+ * 						STDEV_USER_FILENO - Start of user defined
+ *
+ * 	@return	Handle : if succeeded
+ * 			- 1 : Failed
+ */
+int InstallBlkDev(STDDEV *pDev, int MapId);
+
+/*
+ * Remove block device from stdio syscall
+ *
+ * @param	Handle : Handle of device previously installed
+ */
+void RemoveBlkDev(int Handle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	// __STDDEV_H__
+
---
+++ src/stddev.c
@@ -0,0 +1,188 @@
+/*--------------------------------------------------------------------------
+File   : stddev.c
+
+Author : Hoang Nguyen Hoan          Mar. 4, 2015
+
+Desc   : System functions overload for libc
+
+Copyright (c) 2015, I-SYST, all rights reserved
+
+Permission to use, copy, modify, and distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies, and none of the
+names : I-SYST or its contributors may be used to endorse or
+promote products derived from this software without specific prior written
+permission.
+
+For info or contributing contact : hnhoan at i-syst dot com
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----------------------------------------------------------------------------
+Modified by          Date              Description
+
+----------------------------------------------------------------------------*/
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "stddev.h"
+
+#define STDDEV_FDIDX_MASK		0xF
+#define STDDEV_FDIDX_NBITS		4
+
+
+
+STDDEV *g_DevTable[STDDEV_MAX] = {
+	NULL,
+};
+
+int InstallBlkDev(STDDEV *pDev, int MapId)
+{
+	int retval = -1;
+
+	switch (MapId)
+	{
+		case STDIN_FILENO:
+		case STDOUT_FILENO:
+		case STDERR_FILENO:
+		case STDDEV_FATFS_FILENO:
+			retval = MapId;
+			break;
+		default:
+			for (int i = STDDEV_USER_FILENO; i < STDDEV_MAX; i++)
+			{
+				if (g_DevTable[i] == NULL)
+				{
+					retval = i;
+					break;
+				}
+			}
+
+	}
+	if (retval >= 0)
+	{
+		g_DevTable[retval] = pDev;
+	}
+
+	return retval;
+}
+
+void RemoveBlkDev(int Idx)
+{
+	if (Idx >=0 && Idx < STDDEV_MAX)
+		g_DevTable[Idx] = NULL;
+}
+
+int _open(const char *pPathName, int Flags, int Mode)
+{
+//	return g_FatFS.Open((char*)pPathName, Flags, Mode);
+	if (pPathName == NULL)
+		return -1;
+
+	char *p;
+	int retval = -1;
+
+	p = strchr(pPathName, ':');
+
+	if (p == NULL || strncmp(pPathName, "FAT:", 4) == 0)
+	{
+		retval = g_DevTable[STDDEV_FATFS_FILENO]->Open(g_DevTable[STDDEV_FATFS_FILENO]->pDevObj, pPathName, Flags, Mode);
+		if (retval != -1)
+		{
+			retval = (retval << 4) | STDDEV_FATFS_FILENO;
+		}
+	}
+	else
+	{
+		// check for named device
+		for (int i = STDDEV_FATFS_FILENO; i < STDDEV_MAX; i++)
+		{
+			if (strncmp(g_DevTable[i]->Name, pPathName, 4) == 0)
+			{
+				retval = g_DevTable[i]->Open(g_DevTable[i]->pDevObj, pPathName, Flags, Mode);
+
+				if (retval != -1)
+				{
+					retval = (retval << 4) | i;
+				}
+			}
+		}
+	}
+
+	return retval;
+}
+
+int _close(int Fd)
+{
+	int idx = Fd & STDDEV_FDIDX_MASK;
+
+	if (idx < 0 || idx >= STDDEV_MAX)
+		return -1;
+
+	if (idx >= STDDEV_FATFS_FILENO)
+		Fd >>= STDDEV_FDIDX_NBITS;
+
+	if (g_DevTable[idx] && g_DevTable[idx]->Close)
+		return g_DevTable[idx]->Close(g_DevTable[idx]->pDevObj, Fd);
+
+	return -1;
+}
+
+int _lseek(int Fd, int Offset)
+{
+	int idx = Fd & STDDEV_FDIDX_MASK;
+
+	if (idx < 0 || idx >= STDDEV_MAX)
+		return -1;
+
+	if (idx >= STDDEV_FATFS_FILENO)
+		Fd >>= STDDEV_FDIDX_NBITS;
+
+	if (g_DevTable[idx]  && g_DevTable[idx]->Seek)
+		return g_DevTable[idx]->Seek(g_DevTable[idx]->pDevObj, Fd, Offset);
+
+	return -1;
+}
+
+int _read (int Fd, char *pBuff, size_t Len)
+{
+	int idx = Fd & STDDEV_FDIDX_MASK;
+
+	if (idx < 0 || idx >= STDDEV_MAX)
+		return -1;
+
+	if (idx >= STDDEV_FATFS_FILENO)
+		Fd >>= STDDEV_FDIDX_NBITS;
+
+	if (g_DevTable[idx] && g_DevTable[idx]->Read)
+		return g_DevTable[idx]->Read(g_DevTable[idx]->pDevObj, Fd, pBuff, Len);
+
+	return -1;
+}
+
+int _write (int Fd, char *pBuff, size_t Len)
+{
+	int idx = Fd & STDDEV_FDIDX_MASK;
+
+	if (idx < 0 || idx >= STDDEV_MAX)
+		return -1;
+
+	if (idx >= STDDEV_FATFS_FILENO)
+		Fd >>= STDDEV_FDIDX_NBITS;
+
+	if (g_DevTable[idx] && g_DevTable[idx]->Write)
+	{
+		return g_DevTable[idx]->Write(g_DevTable[idx]->pDevObj, Fd, pBuff, Len);
+	}
+
+	return -1;
+}
