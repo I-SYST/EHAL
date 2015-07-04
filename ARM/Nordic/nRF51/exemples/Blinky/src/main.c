72b96d80dc42b03c93331d72cbcf3a6ab0be54de
Hoan Hoang <hnhoan@i-syst.com>
2015-07-03 23:08:12 -0400

    Add RTX version of Blinky

--- ARM/Nordic/nRF51/exemples/Blinky/src/main.c
+++ ARM/Nordic/nRF51/exemples/Blinky/src/main.c
@@ -2,22 +2,29 @@
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
+//#include "idelay.h"
+#include <stdlib.h>
 
-#define LED_0	16
-#define LED_1	17
+#define LED_0	30
+#define LED_1	25
+#define LED_2	28
+
+extern uint32_t SystemMicroSecNopCnt;
 
 int main(void)
 {
-  // flash LED_0 and LED_1 alternately
+
+	// flash LED_0 and LED_1 alternately
   nrf_gpio_cfg_output(LED_0);
   nrf_gpio_cfg_output(LED_1);
+  nrf_gpio_cfg_output(LED_2);
   
   while(true)
   {
     nrf_gpio_pin_clear(LED_0);
-    nrf_gpio_pin_set(LED_1);
+    nrf_gpio_pin_set(LED_2);
     nrf_delay_us(1000000);
-    nrf_gpio_pin_clear(LED_1);
+    nrf_gpio_pin_clear(LED_2);
     nrf_gpio_pin_set(LED_0);
     nrf_delay_us(1000000);
   }
