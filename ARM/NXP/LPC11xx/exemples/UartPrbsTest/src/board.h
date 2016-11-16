684f6118aa16f44ca0d1837d87c1f9980df17902
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2016-11-15 22:42:55 -0500

    LPC11xx uart exemple

---
+++ ARM/NXP/LPC11xx/exemples/UartPrbsTest/src/board.h
@@ -0,0 +1,28 @@
+/*
+ * board.h
+ *
+ *  Created on: Nov 15, 2016
+ *      Author: hoan
+ */
+
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#define UART_RX_PORT				0
+#define UART_RX_PIN					18
+#define UART_RX_PINOP				1
+#define UART_TX_PORT				0
+#define UART_TX_PIN					19
+#define UART_TX_PINOP				1
+#define UART_CTS_PORT				0
+#define UART_CTS_PIN				7
+#define UART_CTS_PINOP				1
+#define UART_RTS_PORT				0
+#define UART_RTS_PIN				17
+#define UART_RTS_PINOP				1
+
+
+
+
+#endif // __BOARD_H__
+
