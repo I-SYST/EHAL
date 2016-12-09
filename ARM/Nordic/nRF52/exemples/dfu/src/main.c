bff65194f20cd4dd56422cb830b4b6a12e3ad368
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2016-12-09 03:51:13 -0500

    DFU nRF52, SDK12.2

---
+++ ARM/Nordic/nRF52/exemples/dfu/src/main.c
@@ -0,0 +1,94 @@
+/* Copyright (c) 2016 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+/** @file
+ *
+ * @defgroup bootloader_secure main.c
+ * @{
+ * @ingroup dfu_bootloader_api
+ * @brief Bootloader project main file for secure DFU.
+ *
+ */
+
+#include <stdint.h>
+#include "boards.h"
+#include "nrf_mbr.h"
+#include "nrf_bootloader.h"
+#include "nrf_bootloader_app_start.h"
+#include "nrf_dfu.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "app_error.h"
+#include "app_error_weak.h"
+#include "nrf_bootloader_info.h"
+
+void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
+{
+    NRF_LOG_ERROR("received a fault! id: 0x%08x, pc: 0x&08x\r\n", id, pc);
+    NVIC_SystemReset();
+}
+
+void app_error_handler_bare(uint32_t error_code)
+{
+    (void)error_code;
+    NRF_LOG_ERROR("received an error: 0x%08x!\r\n", error_code);
+    NVIC_SystemReset();
+}
+
+
+/**@brief Function for initialization of LEDs.
+ */
+static void leds_init(void)
+{
+    bsp_board_leds_init();
+    bsp_board_led_on(BSP_BOARD_LED_2);
+}
+
+
+/**@brief Function for initializing the button module.
+ */
+static void buttons_init(void)
+{
+    nrf_gpio_cfg_sense_input(BOOTLOADER_BUTTON,
+                             BUTTON_PULL,
+                             NRF_GPIO_PIN_SENSE_LOW);
+}
+
+
+/**@brief Function for application main entry.
+ */
+int main(void)
+{
+    uint32_t ret_val;
+
+//    (void) NRF_LOG_INIT(NULL);
+
+    NRF_LOG_INFO("Inside main\r\n");
+
+    leds_init();
+    buttons_init();
+
+    ret_val = nrf_bootloader_init();
+    APP_ERROR_CHECK(ret_val);
+
+    // Either there was no DFU functionality enabled in this project or the DFU module detected
+    // no ongoing DFU operation and found a valid main application.
+    // Boot the main application.
+    nrf_bootloader_app_start(MAIN_APPLICATION_START_ADDR);
+
+    // Should never be reached.
+    NRF_LOG_INFO("After main\r\n");
+}
+
+/**
+ * @}
+ */
