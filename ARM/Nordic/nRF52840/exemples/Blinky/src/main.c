ccf0938813ab9ed9a8c45a18710fb8d394c9ec71
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2017-06-14 22:53:01 -0400

    nRF52840 Blinky example initial commit

---
+++ ARM/Nordic/nRF52840/exemples/Blinky/src/main.c
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "idelay.h"
+#include "blueio_board.h"
+#include "iopincfg.h"
+#include "iopinctrl.h"
+
+extern void initialise_monitor_handles(void);
+
+int main(void)
+{
+   // initialise_monitor_handles();
+
+    printf("hello world!\n");
+
+	IOPinConfig(0, BLUEIO_LED_BLUE_PIN, 0, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL);
+	IOPinSet(0, BLUEIO_LED_BLUE_PIN);
+	IOPinConfig(0, BLUEIO_LED_GREEN_PIN, 0, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL);
+	IOPinSet(0, BLUEIO_LED_GREEN_PIN);
+	IOPinConfig(0, BLUEIO_LED_RED_PIN, 0, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL);
+	IOPinSet(0, 17);
+  
+	while(true)
+	{
+		IOPinClear(0, BLUEIO_LED_BLUE_PIN);
+		usDelay(1000000);
+		IOPinSet(0, BLUEIO_LED_BLUE_PIN);
+		IOPinClear(0, BLUEIO_LED_GREEN_PIN);
+		usDelay(1000000);
+		IOPinSet(0, BLUEIO_LED_GREEN_PIN);
+		IOPinClear(0, BLUEIO_LED_RED_PIN);
+		usDelay(1000000);
+		IOPinSet(0, BLUEIO_LED_RED_PIN);
+		usDelay(1000000);
+	}
+}
+
