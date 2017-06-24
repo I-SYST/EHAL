42de86226e924b6a669034ab3c38a61999dca735
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2017-06-23 23:02:00 -0400

    Add pin sense & drive strength to gpio pin configuration Combine to single iopinctrl for nRF5x serie (including nRF52840 with multiport gpio)

--- ARM/Nordic/include/iopinctrl.h
+++ ARM/Nordic/include/iopinctrl.h
@@ -6,7 +6,7 @@ Author : Hoang Nguyen Hoan          June. 2, 2014
 Desc   : General I/O pin control implementation specific 
 		 This file must be named iopinctrl.h no matter which target
 
-		 This is nRF52 implementation
+		 This is nRF51 implementation
 
 Copyright (c) 2014, I-SYST inc., all rights reserved
 
@@ -38,45 +38,115 @@ Modified by          Date              Description
 #define __IOPINCTRL_H__
 
 #include <stdint.h>
-#include "nrf52.h"
+#include "nrf.h"
 #include "iopincfg.h"
 
 static inline void IOPinSetDir(int PortNo, int PinNo, IOPINDIR Dir)
 {
+	NRF_GPIO_Type *reg = NRF_GPIO;
+
+#ifdef NRF52840_XXAA
+	if (PortNo == 1)
+	{
+		reg = NRF_P1;
+	}
+
+#endif
+
 	if (Dir == IOPINDIR_OUTPUT)
-		NRF_P0->DIRSET = (1 << PinNo);
+		reg->DIRSET = (1 << PinNo);
 	else if (Dir == IOPINDIR_INPUT)
-		NRF_P0->DIRCLR = (1 << PinNo);
+		reg->DIRCLR = (1 << PinNo);
 }
 
 static inline int IOPinRead(int PortNo, int PinNo)
 {
-	return (NRF_P0->IN >> PinNo) & 1;
+	NRF_GPIO_Type *reg = NRF_GPIO;
+
+#ifdef NRF52840_XXAA
+	if (PortNo == 1)
+	{
+		reg = NRF_P1;
+	}
+
+#endif
+
+	return (reg->IN >> PinNo) & 1;
 }
 
 static inline void IOPinSet(int PortNo, int PinNo)
 {
-	NRF_P0->OUTSET = (1 << PinNo);
+	NRF_GPIO_Type *reg = NRF_GPIO;
+
+#ifdef NRF52840_XXAA
+	if (PortNo == 1)
+	{
+		reg = NRF_P1;
+	}
+
+#endif
+
+	reg->OUTSET = (1 << PinNo);
 }
 
 static inline void IOPinClear(int PortNo, int PinNo)
 {
-	NRF_P0->OUTCLR = (1 << PinNo);
+	NRF_GPIO_Type *reg = NRF_GPIO;
+
+#ifdef NRF52840_XXAA
+	if (PortNo == 1)
+	{
+		reg = NRF_P1;
+	}
+
+#endif
+
+	reg->OUTCLR = (1 << PinNo);
 }
 
 static inline void IOPinToggle(int PortNo, int PinNo)
 {
-	NRF_P0->OUT = NRF_P0->OUT ^ (1 << PinNo);
+	NRF_GPIO_Type *reg = NRF_GPIO;
+
+#ifdef NRF52840_XXAA
+	if (PortNo == 1)
+	{
+		reg = NRF_P1;
+	}
+
+#endif
+
+	reg->OUT = NRF_GPIO->OUT ^ (1 << PinNo);
 }
 
 static inline uint32_t IOPinReadPort(int PortNo)
 {
-	return NRF_P0->IN;
+	NRF_GPIO_Type *reg = NRF_GPIO;
+
+#ifdef NRF52840_XXAA
+	if (PortNo == 1)
+	{
+		reg = NRF_P1;
+	}
+
+#endif
+
+	return reg->IN;
 }
 
 static inline void IOPinWritePort(int PortNo, uint32_t Data)
 {
-	NRF_P0->OUT = Data;
+	NRF_GPIO_Type *reg = NRF_GPIO;
+
+#ifdef NRF52840_XXAA
+	if (PortNo == 1)
+	{
+		reg = NRF_P1;
+	}
+
+#endif
+
+	reg->OUT = Data;
 }
 
 #endif	// __IOPINCTRL_H__
