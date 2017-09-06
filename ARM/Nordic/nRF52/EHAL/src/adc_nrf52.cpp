dc04a94b24ac7689c5f7d790f7c0c351a2c8b985
Hoan Hoang <nh.hoang@motsai.com>
2017-09-06 17:18:42 -0400

    more cleanup

--- ARM/Nordic/nRF52/EHAL/src/adc_nrf52.cpp
+++ ARM/Nordic/nRF52/EHAL/src/adc_nrf52.cpp
@@ -70,7 +70,7 @@ extern "C" void SAADC_IRQHandler()
 	if (NRF_SAADC->EVENTS_STARTED)
 	{
 		NRF_SAADC->EVENTS_STARTED = 0;
-		NRF_SAADC->TASKS_SAMPLE = 1;
+        NRF_SAADC->TASKS_SAMPLE = 1;
 	}
 	if (NRF_SAADC->EVENTS_CALIBRATEDONE)
 	{
@@ -80,7 +80,6 @@ extern "C" void SAADC_IRQHandler()
 	}
 	if (NRF_SAADC->EVENTS_DONE)
 	{
-	    //NRF_SAADC->TASKS_SAMPLE = 1;
 	    NRF_SAADC->EVENTS_DONE = 0;
         NRF_SAADC->EVENTS_RESULTDONE = 0;
 	}
@@ -114,19 +113,18 @@ extern "C" void SAADC_IRQHandler()
 					cnt++;
 				}
 			}
-			NRF_SAADC->RESULT.AMOUNT = 0;
-	        NRF_SAADC->EVENTS_RESULTDONE = 0;
-	        NRF_SAADC->EVENTS_DONE = 0;
-	        NRF_SAADC->EVENTS_END = 0;
-	        NRF_SAADC->EVENTS_STARTED = 0;
 
-
-			evt = ADC_EVT_DATA_READY;
+	        evt = ADC_EVT_DATA_READY;
 
 			s_AdcnRF52DevData.pDevObj->EvtHandler(evt);
 		}
+
+        NRF_SAADC->RESULT.AMOUNT = 0;
+        NRF_SAADC->EVENTS_RESULTDONE = 0;
+        NRF_SAADC->EVENTS_DONE = 0;
+        NRF_SAADC->EVENTS_END = 0;
+
 		NRF_SAADC->TASKS_START = 1;
-		//NRF_SAADC->TASKS_SAMPLE = 1;
 	}
 	NVIC_ClearPendingIRQ(SAADC_IRQn);
 }
@@ -560,11 +558,13 @@ bool ADCnRF52::StartConversion()
 	NRF_SAADC->RESULT.PTR = (uint32_t)s_AdcnRF52DevData.ResData;
 	NRF_SAADC->RESULT.MAXCNT = s_AdcnRF52DevData.NbChanAct;
 	NRF_SAADC->TASKS_START = 1;
+
 	if (vbInterrupt == false)
 	{
-	    while (NRF_SAADC->EVENTS_STARTED == 0);
+	    int timeout = 10000;
+	    while (NRF_SAADC->EVENTS_STARTED == 0 && timeout-- > 0);
+        NRF_SAADC->EVENTS_STARTED = 0;
 	    NRF_SAADC->TASKS_SAMPLE = 1;
-	    NRF_SAADC->EVENTS_STARTED = 0;
 	}
 
 	return true;
