120609b5640efe4ad9c666b99e861de1ab35bcbc
Hoan Hoang <hnhoan@i-syst.com>
2019-08-14 16:18:22 -0400

    patch ble scan error invalid state

--- ARM/Nordic/src/ble_app_nrf52.cpp
+++ ARM/Nordic/src/ble_app_nrf52.cpp
@@ -1588,14 +1588,19 @@ void BleAppScan()
 	if (g_BleAppData.bScan == true)
 	{
 		err_code = sd_ble_gap_scan_start(NULL, &g_BleScanReportData);
+		if (err_code == NRF_ERROR_INVALID_STATE)
+		{
+			 err_code = sd_ble_gap_scan_stop();
+			g_BleAppData.bScan = false;
+		}
 	}
-	else
+	if (g_BleAppData.bScan == false)
 	{
 	    g_BleAppData.bScan = true;
 
 		err_code = sd_ble_gap_scan_start(&s_BleScanParams, &g_BleScanReportData);
+		APP_ERROR_CHECK(err_code);
 	}
-	APP_ERROR_CHECK(err_code);
 }
 
 void BleAppScanStop()
