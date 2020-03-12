7fcb32011e76f63c2ce3dceb55a904cc1b9947d5
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2020-03-11 21:54:20 -0400

    nrf_log uart backend example

---
+++ ARM/Nordic/nRF52/exemples/UartnRFLogTest/src/board.h
@@ -0,0 +1,60 @@
+/*
+ * board.h
+ *
+ *  Created on: Nov 15, 2016
+ *      Author: hoan
+ */
+
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include "blueio_board.h"
+
+//#define NORDIC_DK
+//#define NEBLINA
+
+#ifdef NORDIC_DK
+#define UART_RX_PORT		0
+#define UART_RX_PIN			8
+#define UART_RX_PINOP		1
+#define UART_TX_PORT		0
+#define UART_TX_PIN			6
+#define UART_TX_PINOP		1
+#define UART_CTS_PORT		0
+#define UART_CTS_PIN		7
+#define UART_CTS_PINOP		1
+#define UART_RTS_PORT		0
+#define UART_RTS_PIN		5
+#define UART_RTS_PINOP		1
+#elif defined(NEBLINA)
+#define UART_RX_PORT		0
+#define UART_RX_PIN			6
+#define UART_RX_PINOP		1
+#define UART_TX_PORT		0
+#define UART_TX_PIN			7
+#define UART_TX_PINOP		1
+#define UART_CTS_PORT		0
+#define UART_CTS_PIN		29
+#define UART_CTS_PINOP		1
+#define UART_RTS_PORT		0
+#define UART_RTS_PIN		28
+#define UART_RTS_PINOP		1
+#else
+#define UART_NO				0
+#define UART_RX_PORT		BLUEIO_UART_RX_PORT
+#define UART_RX_PIN			BLUEIO_UART_RX_PIN
+#define UART_RX_PINOP		BLUEIO_UART_RX_PINOP
+#define UART_TX_PORT		BLUEIO_UART_TX_PORT
+#define UART_TX_PIN			BLUEIO_UART_TX_PIN
+#define UART_TX_PINOP		BLUEIO_UART_TX_PINOP
+#define UART_CTS_PORT		BLUEIO_UART_CTS_PORT
+#define UART_CTS_PIN		BLUEIO_UART_CTS_PIN
+#define UART_CTS_PINOP		BLUEIO_UART_CTS_PINOP
+#define UART_RTS_PORT		BLUEIO_UART_RTS_PORT
+#define UART_RTS_PIN		BLUEIO_UART_RTS_PIN
+#define UART_RTS_PINOP		BLUEIO_UART_RTS_PINOP
+#endif
+
+
+#endif // __BOARD_H__
+
---
+++ ARM/Nordic/nRF52/exemples/UartnRFLogTest/src/main.cpp
@@ -0,0 +1,107 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Nguyen Hoan Hoang
+// Version     :
+// Copyright   : Copyright (c) 2020, I-SYST inc.
+// Description : Hello World in C++
+//============================================================================
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+#include "nrf_log_backend_uart.h"
+#include "coredev/iopincfg.h"
+#include "board.h"
+
+int nRFUartEvthandler(UARTDEV *pDev, UART_EVT EvtId, uint8_t *pBuffer, int BufferLen);
+
+#define FIFOSIZE			CFIFO_MEMSIZE(64)
+
+uint8_t g_TxBuff[FIFOSIZE];
+
+static IOPINCFG s_UartPins[] = {
+	{UART_RX_PORT, UART_RX_PIN, UART_RX_PINOP, IOPINDIR_INPUT, IOPINRES_NONE, IOPINTYPE_NORMAL},	// RX
+	{UART_TX_PORT, UART_TX_PIN, UART_TX_PINOP, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL},	// TX
+	{UART_CTS_PORT, UART_CTS_PIN, UART_CTS_PINOP, IOPINDIR_INPUT, IOPINRES_NONE, IOPINTYPE_NORMAL},	// CTS
+	{UART_RTS_PORT, UART_RTS_PIN, UART_RTS_PINOP, IOPINDIR_OUTPUT, IOPINRES_NONE, IOPINTYPE_NORMAL},// RTS
+};
+
+// UART configuration data
+static const UARTCFG s_UartCfg = {
+	.DevNo = 0,
+	.pIOPinMap = s_UartPins,
+	.NbIOPins = sizeof(s_UartPins) / sizeof(IOPINCFG),
+	.Rate = 1000000,			// Rate
+	.DataBits = 8,
+	.Parity = UART_PARITY_NONE,
+	.StopBits = 1,					// Stop bit
+	.FlowControl = UART_FLWCTRL_HW,
+	.bIntMode = true,
+	.IntPrio = 1, 					// use APP_IRQ_PRIORITY_LOW with Softdevice
+	.EvtCallback = nRFUartEvthandler,
+	.bFifoBlocking = true,				// fifo blocking mode
+	.RxMemSize = 0,
+	.pRxMem = NULL,
+	.TxMemSize = 0,//FIFOSIZE,
+	.pTxMem = NULL,//g_TxBuff,
+	.bDMAMode = false,
+};
+
+#ifdef DEMO_C
+// For C programming
+UARTDEV g_UartDev;
+#else
+// For C++ object programming
+// UART object instance
+UART g_Uart;
+#endif
+
+int nRFUartEvthandler(UARTDEV *pDev, UART_EVT EvtId, uint8_t *pBuffer, int BufferLen)
+{
+	int cnt = 0;
+	uint8_t buff[20];
+
+	switch (EvtId)
+	{
+		case UART_EVT_RXTIMEOUT:
+		case UART_EVT_RXDATA:
+
+			break;
+		case UART_EVT_TXREADY:
+			break;
+		case UART_EVT_LINESTATE:
+			break;
+	}
+
+	return cnt;
+}
+
+//
+// Print a greeting message on standard output and exit.
+//
+// On embedded platforms this might require semi-hosting or similar.
+//
+// For example, for toolchains derived from GNU Tools for Embedded,
+// to enable semi-hosting, the following was added to the linker:
+//
+// --specs=rdimon.specs -Wl,--start-group -lgcc -lc -lc -lm -lrdimon -Wl,--end-group
+//
+// Adjust it for other toolchains.
+//
+
+int main()
+{
+	g_Uart.Init(s_UartCfg);
+	nrf_log_uart_init(g_Uart);
+
+	NRF_LOG_INIT(NULL);
+	NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+	while (1)
+	{
+	    NRF_LOG_RAW_INFO("NRF_LOG Uart example started.\r\n\r\n");
+	    NRF_LOG_FLUSH();
+	}
+
+	return 0;
+}
