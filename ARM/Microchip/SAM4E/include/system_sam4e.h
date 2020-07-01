48a7878e0490a21a912cfa40625ac35d8d4d5a4e
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2020-06-30 23:10:13 -0400

    SystemInit for SAM4E

--- ARM/Microchip/SAM4E/include/system_sam4e.h
+++ ARM/Microchip/SAM4E/include/system_sam4e.h
@@ -1,54 +1,59 @@
-/**
- * \file
- *
- * \brief Provides the low-level initialization functions that called
- * on chip startup.
- *
- * Copyright (c) 2011-2018 Microchip Technology Inc. and its subsidiaries.
- *
- * \asf_license_start
- *
- * \page License
- *
- * Subject to your compliance with these terms, you may use Microchip
- * software and any derivatives exclusively with Microchip products.
- * It is your responsibility to comply with third party license terms applicable
- * to your use of third party software (including open source software) that
- * may accompany Microchip software.
- *
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
- * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
- * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
- * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
- * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
- * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
- * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
- * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
- * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
- * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
- * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *
- * \asf_license_stop
- *
- */
-/*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
+/**-------------------------------------------------------------------------
+@file	system_sam4e.h
 
-#ifndef SYSTEM_SAM4E_H_INCLUDED
-#define SYSTEM_SAM4E_H_INCLUDED
+@brief	CMSIS System initializations
 
-/* @cond 0 */
-/**INDENT-OFF**/
-#ifdef __cplusplus
-extern "C" {
-#endif
-/**INDENT-ON**/
-/* @endcond */
+
+@author	Hoang Nguyen Hoan
+@date	June. 24, 2020
+
+@license
+
+MIT License
+
+Copyright (c) 2020 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
+#ifndef __SYSTEM_SAM4E_H__
+#define __SYSTEM_SAM4E_H__
 
 #include <stdint.h>
 
-extern uint32_t SystemCoreClock; /* System Clock Frequency (Core Clock) */
+#include "istddef.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief	Set main oscillator type & frequency
+ *
+ * @param	Type : Type of oscillator
+ * 					- OSC_TYPE_RC : internal RC
+ * @param	Freq : Oscillator frequency in Hz
+ *
+ * @return	true - success
+ * 			false - unsupported oscillator or frequency
+ */
+bool SystemSetMainOsc(OSC_TYPE Type, uint32_t Freq);
 
 /**
  * @brief Setup the microcontroller system.
@@ -62,17 +67,11 @@ void SystemInit(void);
  */
 void SystemCoreClockUpdate(void);
 
-/**
- * Initialize flash.
- */
-void system_init_flash(uint32_t dw_clk);
 
-/* @cond 0 */
-/**INDENT-OFF**/
 #ifdef __cplusplus
 }
 #endif
-/**INDENT-ON**/
-/* @endcond */
 
-#endif /* SYSTEM_SAM4E_H_INCLUDED */
+extern uint32_t SystemCoreClock; // System Clock Frequency (Core Clock) in Hz
+
+#endif // __SYSTEM_SAM4E_H__
--- ARM/Microchip/SAM4E/src/system_sam4e.c
+++ ARM/Microchip/SAM4E/src/system_sam4e.c
@@ -1,49 +1,47 @@
-/**
- * \file
- *
- * \brief Provides the low-level initialization functions that called
- * on chip startup.
- *
- * Copyright (c) 2011-2018 Microchip Technology Inc. and its subsidiaries.
- *
- * \asf_license_start
- *
- * \page License
- *
- * Subject to your compliance with these terms, you may use Microchip
- * software and any derivatives exclusively with Microchip products.
- * It is your responsibility to comply with third party license terms applicable
- * to your use of third party software (including open source software) that
- * may accompany Microchip software.
- *
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
- * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
- * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
- * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
- * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
- * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
- * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
- * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
- * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
- * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
- * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *
- * \asf_license_stop
- *
- */
-/*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
+/**-------------------------------------------------------------------------
+@file	system_sam4e.c
+
+@brief	CMSIS System initializations
+
+
+@author	Hoang Nguyen Hoan
+@date	June. 24, 2020
+
+@license
+
+MIT License
+
+Copyright (c) 2020 I-SYST inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+----------------------------------------------------------------------------*/
 
 #include "sam4e.h"
 
-/* @cond 0 */
-/**INDENT-OFF**/
-#ifdef __cplusplus
-extern "C" {
-#endif
-/**INDENT-ON**/
-/* @endcond */
+#include "system_sam4e.h"
+
+#define MAINOSC_FREQ_MIN		3000000
+#define MAINOSC_FREQ_MAX		20000000
+
+#define USB_FREQ				48000000
+#define PLLA_FREQ				240000000
 
 /* Clock Settings (120MHz) */
 #define SYS_BOARD_OSCOUNT   (CKGR_MOR_MOSCXTST(0x8U))
@@ -55,170 +53,220 @@ extern "C" {
 
 #define SYS_CKGR_MOR_KEY_VALUE	CKGR_MOR_KEY_PASSWD/* Key to unlock MOR register */
 
-uint32_t SystemCoreClock = CHIP_FREQ_MAINCK_RC_4MHZ;
+#pragma pack(push, 4)
+typedef struct {
+	OSC_TYPE Type;
+	uint32_t Freq;		//!< in Hz
+} OSC;
+#pragma pack(pop)
 
-/**
- * \brief Setup the microcontroller system.
- * Initialize the System and update the SystemFrequency variable.
- */
-void SystemInit( void )
+static OSC s_MainOsc = {
+	OSC_TYPE_XTAL,
+	12000000
+};
+
+uint32_t SystemCoreClock = 120000000;
+
+
+bool SystemSetMainOsc(OSC_TYPE Type, uint32_t Freq)
 {
-	/* Set FWS according to SYS_BOARD_MCKR configuration */
-	SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(5)|EEFC_FMR_CLOE;
+	if (Freq < MAINOSC_FREQ_MIN || Freq > 20000000)
+	{
+		return false;
+	}
 
-	/* Initialize main oscillator */
-	if ( !(SAM4E_PMC->CKGR_MOR & CKGR_MOR_MOSCSEL) ) {
-		SAM4E_PMC->CKGR_MOR = SYS_CKGR_MOR_KEY_VALUE | SYS_BOARD_OSCOUNT |
-				CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTEN;
+	s_MainOsc.Type = Type;
 
-		while ( !(SAM4E_PMC->PMC_SR & PMC_SR_MOSCXTS) ) {
+	if (Type == OSC_TYPE_RC)
+	{
+		if (Freq < 8000000)
+		{
+			s_MainOsc.Freq = 4000000;
+		}
+		else if (Freq < 16000000)
+		{
+			s_MainOsc.Freq = 8000000;
+		}
+		else
+		{
+			s_MainOsc.Freq = 12000000;
+		}
+	}
+	else
+	{
+		s_MainOsc.Freq = Freq;
+	}
+	SystemInit();
+}
+
+void SystemSetPLLA()
+{
+	uint32_t div = 0;
+	uint32_t mul = 0;
+
+	for (int i = 1; i < 256; i++)
+	{
+		uint32_t freq =  s_MainOsc.Freq / i;
+		if ((PLLA_FREQ % freq) == 0)
+		{
+			div = i;
+			mul = PLLA_FREQ / freq - 1;
+			break;
 		}
 	}
 
-	/* Switch to 3-20MHz Xtal oscillator */
-	SAM4E_PMC->CKGR_MOR = SYS_CKGR_MOR_KEY_VALUE | SYS_BOARD_OSCOUNT |
-			CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTEN |
-			CKGR_MOR_MOSCSEL;
+	SAM4E_PMC->CKGR_PLLAR = CKGR_PLLAR_ONE | CKGR_PLLAR_PLLACOUNT(0x3f) |
+							CKGR_PLLAR_DIVA(div) | CKGR_PLLAR_MULA(mul);
+	while ((SAM4E_PMC->PMC_SR & PMC_SR_LOCKA) == 0);
+}
 
-	while ( !(SAM4E_PMC->PMC_SR & PMC_SR_MOSCSELS) ) {
+void SystemInit()
+{
+	uint32_t mor = 0;
+
+	// WDT is on by default.  Disable it.
+	SAM4E_WDT->WDT_MR = WDT_MR_WDDIS;
+
+	SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(5)|EEFC_FMR_CLOE;
+
+	if (s_MainOsc.Type == OSC_TYPE_RC)
+	{
+		// Internal RC
+		mor = CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCRCEN;
+		switch (s_MainOsc.Freq)
+		{
+			case 8000000:
+				mor |= CKGR_MOR_MOSCRCF_8_MHz;
+				break;
+			case 16000000:
+				mor |= CKGR_MOR_MOSCRCF_12_MHz;
+				break;
+			default:
+				s_MainOsc.Freq = 4000000;
+				break;
+		}
+	}
+	else
+	{
+		// External oscillator
+
+		// Must force to RC before changing to external
+		if (SAM4E_PMC->CKGR_MOR & CKGR_MOR_MOSCSEL)
+		{
+			SAM4E_PMC->CKGR_MOR = CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCXTST(0x8U) |
+					CKGR_MOR_MOSCRCEN;
+
+			while ((SAM4E_PMC->PMC_SR & PMC_SR_MOSCXTS) == 0);
+		}
+		mor = CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCSEL;
 	}
 
+	SAM4E_PMC->CKGR_MOR = mor | CKGR_MOR_MOSCXTST(0x8U);
+	while ((SAM4E_PMC->PMC_SR & PMC_SR_MOSCXTS) == 0);
+
+	// Select MAINCLK
 	SAM4E_PMC->PMC_MCKR = (SAM4E_PMC->PMC_MCKR & ~(uint32_t)PMC_MCKR_CSS_Msk) |
 			PMC_MCKR_CSS_MAIN_CLK;
 
-	while ( !(SAM4E_PMC->PMC_SR & PMC_SR_MCKRDY) ) {
-	}
+	while ((SAM4E_PMC->PMC_SR & PMC_SR_MCKRDY) == 0);
 
-	/* Initialize PLLA */
-	SAM4E_PMC->CKGR_PLLAR = SYS_BOARD_PLLAR;
-	while ( !(SAM4E_PMC->PMC_SR & PMC_SR_LOCKA) ) {
-	}
+	SystemSetPLLA();
 
-	/* Switch to main clock */
-	SAM4E_PMC->PMC_MCKR = (SYS_BOARD_MCKR & ~PMC_MCKR_CSS_Msk) |
-			PMC_MCKR_CSS_MAIN_CLK;
-	while ( !(SAM4E_PMC->PMC_SR & PMC_SR_MCKRDY) ) {
-	}
+	SAM4E_PMC->PMC_MCKR = PMC_MCKR_PRES_CLK_2 | PMC_MCKR_CSS_PLLA_CLK;
+	while ((SAM4E_PMC->PMC_SR & PMC_SR_MCKRDY) );
 
-	/* Switch to PLLA */
-	SAM4E_PMC->PMC_MCKR = SYS_BOARD_MCKR;
-	while ( !(SAM4E_PMC->PMC_SR & PMC_SR_MCKRDY) ) {
-	}
-
-	SystemCoreClock = CHIP_FREQ_CPU_MAX;
+	SystemCoreClockUpdate();
 }
 
 void SystemCoreClockUpdate( void )
 {
-	/* Determine clock frequency according to clock register values */
-	switch (SAM4E_PMC->PMC_MCKR & (uint32_t) PMC_MCKR_CSS_Msk) {
-	case PMC_MCKR_CSS_SLOW_CLK:	/* Slow clock */
-		if ( SAM4E_SUPC->SUPC_SR & SUPC_SR_OSCSEL ) {
-			SystemCoreClock = CHIP_FREQ_XTAL_32K;
-		} else {
-			SystemCoreClock = CHIP_FREQ_SLCK_RC;
-		}
-		break;
+	uint32_t clk = 0;
+	uint32_t mckr = SAM4E_PMC->PMC_MCKR;
+	uint32_t tmp = 0;
 
-	case PMC_MCKR_CSS_MAIN_CLK:	/* Main clock */
-		if ( SAM4E_PMC->CKGR_MOR & CKGR_MOR_MOSCSEL ) {
-			SystemCoreClock = CHIP_FREQ_XTAL_12M;
-		} else {
-			SystemCoreClock = CHIP_FREQ_MAINCK_RC_4MHZ;
-
-			switch ( SAM4E_PMC->CKGR_MOR & CKGR_MOR_MOSCRCF_Msk ) {
-			case CKGR_MOR_MOSCRCF_4_MHz:
-				break;
-
-			case CKGR_MOR_MOSCRCF_8_MHz:
-				SystemCoreClock *= 2U;
-				break;
-
-			case CKGR_MOR_MOSCRCF_12_MHz:
-				SystemCoreClock *= 3U;
-				break;
-
-			default:
-				break;
+	switch (mckr & PMC_MCKR_CSS_Msk)
+	{
+		case PMC_MCKR_CSS_SLOW_CLK:	// SLCK
+			if ( SAM4E_SUPC->SUPC_SR & SUPC_SR_OSCSEL)
+			{
+				clk = 32768;
 			}
-		}
-		break;
-
-	case PMC_MCKR_CSS_PLLA_CLK:	/* PLLA clock */
-		if ( SAM4E_PMC->CKGR_MOR & CKGR_MOR_MOSCSEL ) {
-			SystemCoreClock = CHIP_FREQ_XTAL_12M ;
-		} else {
-			SystemCoreClock = CHIP_FREQ_MAINCK_RC_4MHZ;
-
-			switch ( SAM4E_PMC->CKGR_MOR & CKGR_MOR_MOSCRCF_Msk ) {
-			case CKGR_MOR_MOSCRCF_4_MHz:
-				break;
-
-			case CKGR_MOR_MOSCRCF_8_MHz:
-				SystemCoreClock *= 2U;
-				break;
-
-			case CKGR_MOR_MOSCRCF_12_MHz:
-				SystemCoreClock *= 3U;
-				break;
-
-			default:
-				break;
+			else
+			{
+				clk = 32000;
 			}
-		}
-
-		if ((uint32_t) (SAM4E_PMC->PMC_MCKR & (uint32_t) PMC_MCKR_CSS_Msk) == PMC_MCKR_CSS_PLLA_CLK)
-		{
-			SystemCoreClock *= ((((SAM4E_PMC->CKGR_PLLAR) & CKGR_PLLAR_MULA_Msk) >> CKGR_PLLAR_MULA_Pos) + 1U);
-			SystemCoreClock /= ((((SAM4E_PMC->CKGR_PLLAR) & CKGR_PLLAR_DIVA_Msk) >> CKGR_PLLAR_DIVA_Pos));
-		}
 		break;
 
-	default:
-		break;
-	}
+		case PMC_MCKR_CSS_MAIN_CLK:	// MAINCK
+			if (SAM4E_PMC->CKGR_MOR & CKGR_MOR_MOSCSEL)
+			{
+				clk = s_MainOsc.Freq;
+			}
+			else
+			{
+				switch (SAM4E_PMC->CKGR_MOR & CKGR_MOR_MOSCRCF_Msk)
+				{
+					case CKGR_MOR_MOSCRCF_4_MHz:
+						clk = 4000000;
+						break;
 
-	if ((SAM4E_PMC->PMC_MCKR & PMC_MCKR_PRES_Msk) == PMC_MCKR_PRES_CLK_3) {
-		SystemCoreClock /= 3U;
-	} else {
-		SystemCoreClock >>= ((SAM4E_PMC->PMC_MCKR & PMC_MCKR_PRES_Msk) >>
-				PMC_MCKR_PRES_Pos);
-	}
-}
+					case CKGR_MOR_MOSCRCF_8_MHz:
+						clk = 8000000;
+						break;
 
-/**
- * Initialize flash.
- */
-void system_init_flash( uint32_t ul_clk )
-{
-	/* Set FWS for embedded Flash access according to operating frequency */
-	if ( ul_clk < CHIP_FREQ_FWS_0 ) {
-		SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(0)|EEFC_FMR_CLOE;
-	} else {
-		if (ul_clk < CHIP_FREQ_FWS_1) {
-			SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(1)|EEFC_FMR_CLOE;
-		} else {
-			if (ul_clk < CHIP_FREQ_FWS_2) {
-				SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(2)|EEFC_FMR_CLOE;
-			} else {
-				if ( ul_clk < CHIP_FREQ_FWS_3 ) {
-					SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(3)|EEFC_FMR_CLOE;
-				} else {
-					if ( ul_clk < CHIP_FREQ_FWS_4 ) {
-						SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(4)|EEFC_FMR_CLOE;
-					} else {
-						SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(5)|EEFC_FMR_CLOE;
-					}
+					case CKGR_MOR_MOSCRCF_12_MHz:
+						clk = 12000000;
+						break;
+
+					default:
+						break;
 				}
 			}
-		}
+			break;
+
+		case PMC_MCKR_CSS_PLLA_CLK:	// PLLACK
+			clk = s_MainOsc.Freq;
+			tmp = SAM4E_PMC->CKGR_PLLAR;
+			clk *= ((tmp & CKGR_PLLAR_MULA_Msk) >> CKGR_PLLAR_MULA_Pos) + 1U;
+			clk /= ((tmp & CKGR_PLLAR_DIVA_Msk) >> CKGR_PLLAR_DIVA_Pos);
+			break;
+	}
+
+	mckr &= PMC_MCKR_PRES_Msk;
+	if (mckr == PMC_MCKR_PRES_CLK_3)
+	{
+		clk /= 3;
+	}
+	else
+	{
+		clk >>= (mckr >> PMC_MCKR_PRES_Pos);
+	}
+
+	SystemCoreClock = clk;
+
+	if (SystemCoreClock < CHIP_FREQ_FWS_0)
+	{
+		SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(0) | EEFC_FMR_CLOE;
+	}
+	else if (SystemCoreClock < CHIP_FREQ_FWS_1)
+	{
+		SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(1) | EEFC_FMR_CLOE;
+	}
+	else if (SystemCoreClock < CHIP_FREQ_FWS_2)
+	{
+		SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(2) | EEFC_FMR_CLOE;
+	}
+	else if (SystemCoreClock < CHIP_FREQ_FWS_3)
+	{
+		SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(3) | EEFC_FMR_CLOE;
+	}
+	else if (SystemCoreClock < CHIP_FREQ_FWS_4)
+	{
+		SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(4) | EEFC_FMR_CLOE;
+	}
+	else
+	{
+		SAM4E_EFC->EEFC_FMR = EEFC_FMR_FWS(5) | EEFC_FMR_CLOE;
 	}
 }
 
-/* @cond 0 */
-/**INDENT-OFF**/
-#ifdef __cplusplus
-}
-#endif
-/**INDENT-ON**/
-/* @endcond */
